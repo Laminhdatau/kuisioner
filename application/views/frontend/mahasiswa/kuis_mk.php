134683285f85c5b437a7a6cd7d63e409722a0be7
Laminhdatau <Laminhdataulamin2@gmail.com>
2022-10-18 15:58:03 +0800

    Merubah Tampilan QUis

--- application/views/frontend/mahasiswa/kuis_mk.php
+++ application/views/frontend/mahasiswa/kuis_mk.php
@@ -28,31 +28,93 @@
                   <div class="x_content">
                       
                   <!-- =============================== -->
-                      <div class="col-md-5 col-sm-6  ">
+                      <div class="col-md-12 col-sm-12">
                         <div class="x_panel">
                         <div class="x_title">
-                        <h2>Daily active users <small>Sessions</small></h2>
-                        <ul class="nav navbar-right panel_toolbox">
-                          <li>
-                            <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                          </li>
-                          <li class="dropdown">
-                              
-                          </li>
-                          <li>
-                            <a class="close-link"><i class="fa fa-close"></i></a>
-                          </li>
-                        </ul>
                         <div class="clearfix"></div>
-                        </div>
-                          <div class="x_content">
+
+                        </div >
                             <div class="bs-example" data-example-id="simple-jumbotron">
-                              <div class="jumbotron">
-                                <h1>Hello, world!</h1>
-                                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                              <div class="jumbotron jumbotron-fluid row-1 text-center">
+                                <h3>Kuisoner Evaluasi Matakulia (IPM)</h3>
+                                
+                                    
+                                      <div class="alert alert-dark ">
+                                        <form action="">
+                                          <label for="">Periode</label>
+                                          <select name="" id="">
+                                            <option value="">Genap</option>
+                                            <option value="">Ganjil</option>
+                                          </select>
+
+                                          <select name="" id="">
+                                            <option value="">2021</option>
+                                            <option value="">2022</option>
+                                            <option value="">2023</option>
+                                          </select>
+                                          <button class="badge badge-success">Tampil</button>
+                                        </form>
+                                      </div>
+
+                                    <h3>Riwayat Pengisian Kuisioner</h3>
+                                    <h5><b>Info:</b> Data diupdate setiap jam</h5>
+                                    <form action="">
+                                      <!-- ================================= -->
+                                      <table class="table table-striped">
+                                      <thead class=""> 
+                                           <tr>
+                                              <th scope="col" rowspan="2">NO</th>
+                                              <th scope="col" rowspan="2">MATA KULIAH</th>
+                                              <th scope="col" rowspan="2">DOSEN PENGAMPU</th>
+                                              <th scope="col" colspan="2">KUISIONER</th>
+                                              <tr>
+                                                  <th>DOSEN</th>
+                                                  <th>MATA KULIAH</th>
+                                              </tr>
+                                            </tr>
+                                      </thead>
+                                      <tbody>
+                                          <tr>
+                                            <td>1</td>
+                                            <td>FM1</td>
+                                            <td width="55%">SAIFUL BAHRI MUSA<td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                          </tr>
+                                              <tr>
+                                            <td>1</td>
+                                            <td>FM1</td>
+                                            <td width="55%">SAIFUL BAHRI MUSA<td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                          </tr>
+                                          </tr>
+                                              <tr>
+                                            <td>1</td>
+                                            <td>FM1</td>
+                                            <td width="55%">SAIFUL BAHRI MUSA<td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                            <td>
+                                              <a href="#" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                            </td>
+                                          </tr>
+                                      </tbody>
+                                      </table>
+                                    <!-- ========================================= -->
+                                    </form>
                               </div>
+                              
                             </div>
-                          </div>
                         </div>
                       </div>
 
