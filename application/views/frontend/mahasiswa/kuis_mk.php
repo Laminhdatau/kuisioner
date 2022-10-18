7a5402a22d4543e57e7bdeec46de2708e5d787b7
Laminhdatau <Laminhdataulamin2@gmail.com>
2022-10-18 14:58:25 +0800

    Menyesuaikan Tampilan quis dosen

---
+++ application/views/frontend/mahasiswa/kuis_mk.php
@@ -0,0 +1,66 @@
+. <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Pengisian Kuisioner</h3>
+              </div>
+
+              
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12">
+                <div class="x_panel ">
+                  <div class="x_title">
+                    <h2>Halaman Kuisioner Mata Kuliah</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      
+                  <!-- =============================== -->
+                      <div class="col-md-5 col-sm-6  ">
+                        <div class="x_panel">
+                        <div class="x_title">
+                        <h2>Daily active users <small>Sessions</small></h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                          <li>
+                            <a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                          </li>
+                          <li class="dropdown">
+                              
+                          </li>
+                          <li>
+                            <a class="close-link"><i class="fa fa-close"></i></a>
+                          </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                        </div>
+                          <div class="x_content">
+                            <div class="bs-example" data-example-id="simple-jumbotron">
+                              <div class="jumbotron">
+                                <h1>Hello, world!</h1>
+                                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                  <!-- ================================= -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
