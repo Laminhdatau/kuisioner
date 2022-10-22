f89b661d6cd57aa59a1c8498f0e86bd1f4cc31f0
Lamin H. Datau <laminhdataulamin2@gmail.com>
2022-10-23 01:02:23 +0700

    update db

--- db/db_siakad.sql
+++ db/db_siakad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 14, 2022 at 07:11 PM
+-- Generation Time: Oct 23, 2022 at 01:01 AM
 -- Server version: 5.7.39-0ubuntu0.18.04.2
 -- PHP Version: 7.2.24-0ubuntu0.18.04.13
 
@@ -51,7 +51,7 @@ CREATE TABLE `t_answer_quisioner` (
   `kd_answer_quisioner` varchar(255) NOT NULL COMMENT 'UUID',
   `nim` varchar(15) NOT NULL,
   `kd_mk` int(11) NOT NULL,
-  `kd_dosen_pengampu` varchar(255) NOT NULL,
+  `nipy` varchar(255) NOT NULL,
   `kd_quisioner` varchar(255) NOT NULL,
   `id_answer` int(11) NOT NULL,
   `comments` text NOT NULL,
@@ -62,8 +62,8 @@ CREATE TABLE `t_answer_quisioner` (
 -- Dumping data for table `t_answer_quisioner`
 --
 
-INSERT INTO `t_answer_quisioner` (`kd_answer_quisioner`, `nim`, `kd_mk`, `kd_dosen_pengampu`, `kd_quisioner`, `id_answer`, `comments`, `waktu`) VALUES
-('', '20501049', 1, '00102', '0a090997-3f24-11ed-84af-3c0c898d271c', 1, 'kerem', '2022-10-07 11:29:21');
+INSERT INTO `t_answer_quisioner` (`kd_answer_quisioner`, `nim`, `kd_mk`, `nipy`, `kd_quisioner`, `id_answer`, `comments`, `waktu`) VALUES
+('2112wsxssdfcdfdsfff', '20501049', 1, '121211', '0a090997-3f24-11ed-84af-3c0c898d271c', 1, 'sangat memuaskan kita', '2022-10-04 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -72,18 +72,25 @@ INSERT INTO `t_answer_quisioner` (`kd_answer_quisioner`, `nim`, `kd_mk`, `kd_dos
 --
 
 CREATE TABLE `t_dosen_pengampu` (
-  `kd_dosen_pengampu` varchar(255) NOT NULL,
-  `nama_dosen` varchar(255) NOT NULL,
-  `id_role` int(11) NOT NULL
+  `nipy` varchar(25) NOT NULL,
+  `nama_dosen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `t_dosen_pengampu`
 --
 
-INSERT INTO `t_dosen_pengampu` (`kd_dosen_pengampu`, `nama_dosen`, `id_role`) VALUES
-('00101', 'Saiful Bahri Musa', 2),
-('00102', 'Ismail Mohidin', 2);
+INSERT INTO `t_dosen_pengampu` (`nipy`, `nama_dosen`) VALUES
+('121211', 'Saiful Bahri Musa'),
+('121212', 'Ismail Mohidin'),
+('121213', 'Alimudin'),
+('121214', 'fajar hermawanto'),
+('121215', 'Salman Suleman'),
+('121216', 'Irwan Karim'),
+('121217', 'Saprina mamase'),
+('121218', 'Nursetya'),
+('121219', 'Zainudin husain'),
+('121220', 'Franky Tupamahu');
 
 -- --------------------------------------------------------
 
@@ -112,17 +119,16 @@ INSERT INTO `t_jenis_quisioner` (`id_jenis_quisioner`, `jenis_quisioner`) VALUES
 
 CREATE TABLE `t_mahasiswa` (
   `nim` varchar(15) NOT NULL,
-  `nama` varchar(150) NOT NULL,
-  `id_role` int(11) NOT NULL
+  `nama` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `t_mahasiswa`
 --
 
-INSERT INTO `t_mahasiswa` (`nim`, `nama`, `id_role`) VALUES
-('20501049', 'Lamin H. Datau', 3),
-('20501050', 'Marno Biki', 3);
+INSERT INTO `t_mahasiswa` (`nim`, `nama`) VALUES
+('20501049', 'Lamin H. Datau'),
+('20501050', 'Marno Biki');
 
 -- --------------------------------------------------------
 
@@ -140,8 +146,8 @@ CREATE TABLE `t_mk` (
 --
 
 INSERT INTO `t_mk` (`kd_mk`, `mk`) VALUES
-(1, 'TI'),
-(2, 'THP');
+(1, 'Framework'),
+(2, 'Mobile');
 
 -- --------------------------------------------------------
 
@@ -162,9 +168,9 @@ CREATE TABLE `t_quisioner` (
 --
 
 INSERT INTO `t_quisioner` (`kd_quisioner`, `quisioner`, `id_jenis_quisioner`, `status`, `created_up`) VALUES
-('05d73bdf-3f26-11ed-84af-3c0c898d271c', 'Apakah capaian pembelajaran /learning outcome dijelaskan pada setiap pergantian tahap kuliah?', 2, '1', '2022-10-03 10:57:55'),
+('05d73bdf-3f26-11ed-84af-3c0c898d271c', 'Apakah capaian pembelajaran /learning outcome dijelaskan pada setiap pergantian tahap kuliah?', 2, '1', '2022-10-16 06:10:05'),
 ('0a090997-3f24-11ed-84af-3c0c898d271c', 'Apakah rencana pembelajaran(rp) dapat diakses dari laman resmi prodi atau di dalam classroom?', 2, '1', '2022-09-28 11:52:33'),
-('181d72e3-3f26-11ed-84af-3c0c898d271c', 'Apakah pembelajaran scl digunakan? Yaitu mahasiswa diberi kesempatan luas dalammenentukan pembelajaran mandiri.', 2, '1', '2022-09-28 12:07:16'),
+('181d72e3-3f26-11ed-84af-3c0c898d271c', 'Apakah pembelajaran scl digunakan? Yaitu mahasiswa diberi kesempatan luas dalam menentukan pembelajaran mandiri.', 2, '1', '2022-10-16 04:04:17'),
 ('35151377-3f26-11ed-84af-3c0c898d271c', 'Bagaimana kesesuaian sumber belajar yang digunakan mendukung capaian pembelajaran mk?', 2, '1', '2022-09-28 12:08:05'),
 ('41efb8f2-3f26-11ed-84af-3c0c898d271c', 'Bagaimana kesesuaian waktu untuk mengerjakan tugas dengan beban sks MK?', 2, '1', '2022-09-28 12:08:26'),
 ('4dfe87ef-3f26-11ed-84af-3c0c898d271c', 'Bagaimana kesesuaian jenis test / ujian / asesmen lain dengan pemenuhan capaian pembelajaran ?', 2, '1', '2022-09-28 12:08:46'),
@@ -177,7 +183,7 @@ INSERT INTO `t_quisioner` (`kd_quisioner`, `quisioner`, `id_jenis_quisioner`, `s
 ('ad262863-3f23-11ed-84af-3c0c898d271c', 'Dosen memberikan tes/tugas/bentuk asesmen yang lain sesuai dengan tujuan pembelajaran yang telah ditetapkan', 1, '1', '2022-09-28 11:49:57'),
 ('c61c3eca-3f23-11ed-84af-3c0c898d271c', 'Dosen memberikan tugas yang terkait dengan praktek keprofesionalan setelah lulus', 1, '1', '2022-09-28 11:50:39'),
 ('cfa16057-3f23-11ed-84af-3c0c898d271c', 'Dosen memberikan tugas untuk meningkatkan kemampuan bekerja secara kelompok dengan menggunakan media online dan/atau offline', 1, '1', '2022-09-28 11:50:55'),
-('d9bf4073-3ff4-11ed-81ae-503eaa456e2a', 'Dosen mempunyai persepsi positif ats kemampuan mahasiswa saat berinteraksi secara online dan/atau offline', 1, '1', '2022-09-29 12:47:17'),
+('d9bf4073-3ff4-11ed-81ae-503eaa456e2a', 'Dosen mempunyai persepsi positif atas kemampuan mahasiswa saat berinteraksi secara online dan/atau offline', 1, '1', '2022-10-16 04:03:47'),
 ('dbfdf2f4-3f23-11ed-84af-3c0c898d271c', 'Dosen membantu mahasiswa untuk meningkatkan rasa percaya diri, melalui media online dan/ atau offline', 1, '1', '2022-09-28 11:51:16'),
 ('ec292fcd-3f22-11ed-84af-3c0c898d271c', 'Dosen mampu meningkatkan kemampuan mahasiswa untuk mencapai capaian pembelajaran', 1, '1', '2022-09-28 11:44:34'),
 ('f3695c85-3f23-11ed-84af-3c0c898d271c', 'Apakah rencana pembelajaran(rp) dijelaskan di awal perkuliahan?', 2, '1', '2022-09-28 11:51:55'),
@@ -190,20 +196,20 @@ INSERT INTO `t_quisioner` (`kd_quisioner`, `quisioner`, `id_jenis_quisioner`, `s
 --
 
 CREATE TABLE `t_role` (
-  `id_role` int(11) NOT NULL,
-  `role` varchar(20) NOT NULL
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `t_role`
 --
 
-INSERT INTO `t_role` (`id_role`, `role`) VALUES
+INSERT INTO `t_role` (`id`, `role`) VALUES
 (1, 'direktur'),
-(2, 'dosen'),
-(3, 'mahasiswa'),
-(4, 'upm'),
-(5, 'prodi');
+(2, 'mahasiswa'),
+(3, 'upm'),
+(4, 'admin prodi'),
+(5, 'dosen pengampu');
 
 --
 -- Indexes for dumped tables
@@ -223,15 +229,14 @@ ALTER TABLE `t_answer_quisioner`
   ADD KEY `kd_soal_quisioner` (`kd_quisioner`),
   ADD KEY `id_answer` (`id_answer`),
   ADD KEY `nim` (`nim`),
-  ADD KEY `kd_dosen_pengampu` (`kd_dosen_pengampu`),
+  ADD KEY `kd_dosen_pengampu` (`nipy`),
   ADD KEY `kd_mk` (`kd_mk`);
 
 --
 -- Indexes for table `t_dosen_pengampu`
 --
 ALTER TABLE `t_dosen_pengampu`
-  ADD PRIMARY KEY (`kd_dosen_pengampu`),
-  ADD KEY `id_role` (`id_role`);
+  ADD PRIMARY KEY (`nipy`);
 
 --
 -- Indexes for table `t_jenis_quisioner`
@@ -243,8 +248,7 @@ ALTER TABLE `t_jenis_quisioner`
 -- Indexes for table `t_mahasiswa`
 --
 ALTER TABLE `t_mahasiswa`
-  ADD PRIMARY KEY (`nim`),
-  ADD KEY `id_role` (`id_role`);
+  ADD PRIMARY KEY (`nim`);
 
 --
 -- Indexes for table `t_mk`
@@ -263,7 +267,7 @@ ALTER TABLE `t_quisioner`
 -- Indexes for table `t_role`
 --
 ALTER TABLE `t_role`
-  ADD PRIMARY KEY (`id_role`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -283,7 +287,7 @@ ALTER TABLE `t_mk`
 -- AUTO_INCREMENT for table `t_role`
 --
 ALTER TABLE `t_role`
-  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -292,11 +296,11 @@ ALTER TABLE `t_role`
 -- Constraints for table `t_answer_quisioner`
 --
 ALTER TABLE `t_answer_quisioner`
-  ADD CONSTRAINT `t_answer_quisioner_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `t_mahasiswa` (`nim`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_answer_quisioner_ibfk_1` FOREIGN KEY (`id_answer`) REFERENCES `t_answer` (`id_answer`) ON UPDATE CASCADE,
   ADD CONSTRAINT `t_answer_quisioner_ibfk_2` FOREIGN KEY (`kd_quisioner`) REFERENCES `t_quisioner` (`kd_quisioner`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_answer_quisioner_ibfk_4` FOREIGN KEY (`kd_dosen_pengampu`) REFERENCES `t_dosen_pengampu` (`kd_dosen_pengampu`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_answer_quisioner_ibfk_6` FOREIGN KEY (`kd_mk`) REFERENCES `t_mk` (`kd_mk`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `t_answer_quisioner_ibfk_7` FOREIGN KEY (`id_answer`) REFERENCES `t_answer` (`id_answer`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `t_answer_quisioner_ibfk_3` FOREIGN KEY (`nim`) REFERENCES `t_mahasiswa` (`nim`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_answer_quisioner_ibfk_5` FOREIGN KEY (`kd_mk`) REFERENCES `t_mk` (`kd_mk`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_answer_quisioner_ibfk_6` FOREIGN KEY (`nipy`) REFERENCES `t_dosen_pengampu` (`nipy`);
 
 --
 -- Constraints for table `t_quisioner`
